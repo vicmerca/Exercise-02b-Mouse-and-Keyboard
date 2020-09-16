3400a2a0bc1a17f580f6cb604098f52c21696176
vicmerca <vicmerca@iu.edu>
2020-09-15 23:23:18 -0400

    did assignment

--- Player.gd
+++ Player.gd
@@ -12,6 +12,10 @@ func _ready():
 	max_range = get_viewport().size
 
 func _physics_process(delta):
+	var r = get_viewport().get_mouse_position().x
+	midpoint = position.x
+	if abs(r - midpoint) > margin:
+		rotation_degrees += (r - midpoint)*rot_amount
 
 	velocity += get_input()*acc_amount
 	position += velocity
@@ -20,4 +24,12 @@ func _physics_process(delta):
 
 func get_input():
 	var input_dir = Vector2(0,0)
+	if Input.is_action_pressed("up"):
+		input_dir.y -= 1
+	if Input.is_action_pressed("down"):
+		input_dir.y += 1
+	if Input.is_action_pressed("left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("right"):
+		input_dir.x += 1
 	return input_dir.rotated(rotation)
